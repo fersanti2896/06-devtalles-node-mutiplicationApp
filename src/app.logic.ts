ac0df2600d9e9b9417b50c43d3e7342a3b385ee3
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-03 02:04:04 -0600

    Usando yargs en app.logic

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,19 +1,23 @@
+import { yarg } from './config/plugins/args.plugin';
 import fs from 'fs';
 
+const { b:base, l:limit, show } = yarg;
+
 let outputMessage = '';
-const base = 5;
+
 const headerMessage = `
 ==========================
     Tabla del ${ base }
 ==========================\n
 `;
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= limit; i++) {
     outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
 }
 
 outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
+
+if(show) console.log(outputMessage);
 
 const outputPath = `outputs`;
 
